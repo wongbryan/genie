619703365da9efeaa5e95fa0736dcc74c1d8c7a1
Bryan Wong <bryanw0ng@ucla.edu>
2018-05-22 21:20:07 -0700

    Server returns a dummy json each time

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { spawn } = require('child_process')
 const app = express()
+const componentTypes = [  
+  'Button',
+  'Image',
+  'h1',
+  'h2',
+  'h3',
+  'SearchBar',
+  'Paragraph',
+  'InlineStepper'
+]
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -14,13 +24,14 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.post('/api', async (req, res) => {
+app.post('/display', async (req, res) => {
 	const child = spawn('python3', ['/Users/bryanwong/Desktop/dev/genie/dummy.py']);
 	child.on('exit', (code, signal) => {
-		console.log('child process exited with exit code ' + code + '.');
 		const status = code ? 200 : 400;
+		const components = componentTypes.sort(()=>{ return .5 > Math.random() });
 		const payload = {
 			status: status,
+			components: components.slice( Math.floor(componentTypes.length * Math.random()))
 		};
 		res.status(status).send(payload);
 	});
